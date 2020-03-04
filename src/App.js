a13ee2b340543adb4c74d7c2b85be4447daf92ff
Kay Zhao <sqkzhao@gmail.com>
2020-03-03 20:32:53 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/person_card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName="Doe" lastName="Jane" age={45} hairColor="Black"/>
+      <PersonCard firstName="Smith" lastName="John" age={88} hairColor="Brown"/>
+      <PersonCard firstName="Fillmore" lastName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard firstName="Smith" lastName="Maria" age={62} hairColor="Brown"/>
     </div>
   );
 }
