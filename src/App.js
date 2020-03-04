faf1f4e5078283a7684a82b6daa8169dd6343607
Kay Zhao <sqkzhao@gmail.com>
2020-03-03 20:56:31 -0800

    updated props

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/person_card';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName="Doe" lastName="Jane" age={45} hairColor="Black"/>
-      <PersonCard firstName="Smith" lastName="John" age={88} hairColor="Brown"/>
-      <PersonCard firstName="Fillmore" lastName="Millard" age={50} hairColor="Brown"/>
-      <PersonCard firstName="Smith" lastName="Maria" age={62} hairColor="Brown"/>
+      <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black"/>
+      <PersonCard lastName="Smith" firstName="John" age={88} hairColor="Brown"/>
+      <PersonCard lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard lastName="Smith" firstName="Maria" age={62} hairColor="Brown"/>
     </div>
   );
 }
